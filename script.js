f7ed60b1d040fd8f31d5b1dc6355c741f5ed5456
levyguy0 <103501989+levyguy0@users.noreply.github.com>
2022-09-20 20:49:07 +0100

    done

--- script.js
+++ script.js
@@ -2,10 +2,45 @@ const container = document.querySelector('.container');
 
 
 
-for(i = 1;i<257;i++){
+
+
+let width = prompt("How many squares wide do you want the grid to be? 1-50");
+let height = prompt("How many squares high do you want the grid to be? 1-50");
+
+if(width > 50 || height > 50) {
+    alert('Error! Number is too Large!')
+
+
+}
+const gridsize = width*height + 1;
+
+for(i = 1;i<gridsize;i++){
     const content = document.createElement('div');
-    content.classList.add('grid-item');
+    content.classList.add('griditem');
     container.appendChild(content);
     content.textContent = '    ';
 }
+let text = 'auto ';
+let result = text.repeat(width);
+container.style.gridTemplateColumns = result;
+
+
+
+container.addEventListener('mouseover', function (e) {
+    if (e.target.matches('.griditem')) {
+      e.target.classList.add('active');
+    }
+  });
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click',() =>{
+    const items = document.querySelectorAll('.griditem')
+    items.forEach(elem => {
+        elem.classList.remove('active')
+       elem.style.color = 'white'
+       })
+    
+})
+
 
